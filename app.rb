add40ec93bf8f3952898cd5a30d3f2d6521ec8ff
Valerio Franchi <vlrfranchi1@gmail.com>
2022-12-02 16:21:26 +0000

    app

--- app.rb
+++ app.rb
@@ -1,10 +1,26 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative './lib/peep'
+require './database_connection_setup'
 
-class Application < Sinatra::Base
-  # This allows the app code to refresh
-  # without having to restart the server.
+class ChitterWeb < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
   end
+
+  enable :sessions
+
+  get '/peeps' do
+    @peeps = Peep.all
+
+    erb :'peeps/index'
+  end
+
+  post '/peeps/post' do
+    Peep.create(peep: params[:peep])
+
+    redirect '/peeps'
+  end
+
+  run! if app_file == $0
 end
